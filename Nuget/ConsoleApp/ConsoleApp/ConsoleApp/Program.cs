ee537416a30ebfc6d7fbe154b11b06dd9bd21966
Gabriel Gonzalez <gabrielnoise@gmail.com>
2015-07-12 15:29:46 +0100

    creation of the project

---
+++ Nuget/ConsoleApp/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NugetPackageExample;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var writer = new NugetPackageExample.Writer();
+            //version 1.0
+            writer.WriteMessageInConsole("Hello");
+            writer.WritePersianDateTime();
+           //added in version 1.1
+            writer.WriteDayOfWeek();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Nuget/NugetPackageExample/NugetPackageExample/NugetPackageExample/Writer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NugetPackageExample
+{
+    public class Writer
+    {
+        public void WriteMessageInConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void WritePersianDateTime()
+        {
+            Console.WriteLine(PersianDateTime.Now.ToString());
+        }
+
+        public void WriteDayOfWeek()
+        {
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+        }
+    }
+}
