6dfcd99d9c4a688004580306fefa60152e7576b4
Gabriel Gonzalez <gabrielnoise@gmail.com>
2015-08-01 16:56:47 +0100

    Project Creation

---
+++ DecoratorUsingDIContainer/BusinessLayer/LogApplicationFacade.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace DecoratorDesignPattern
+{
+    //Decorator class that add logging functionality to the Application Facade
+    public class LogApplicationFacade : IApplicationFacade
+    {
+        private readonly IApplicationFacade _applicationFacadeOriginal;
+
+        public LogApplicationFacade(IApplicationFacade applicationFacadeOriginal)
+        {
+            _applicationFacadeOriginal = applicationFacadeOriginal;
+        }
+
+        public int Sum(int number1, int number2)
+        {
+            Debug.WriteLine("Method Sum Called");
+            var result  =_applicationFacadeOriginal.Sum(number1, number2);
+            return result;
+        }
+    }
+}
---
+++ DecoratorUsingDIContainer/BusinessLayer/ValidationApplicationFacade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DecoratorDesignPattern
+{
+    public class ValidationApplicationFacade : IApplicationFacade
+    {
+        private readonly IApplicationFacade _applicationFacade;
+
+        public ValidationApplicationFacade(IApplicationFacade applicationFacade)
+        {
+            _applicationFacade = applicationFacade;
+        }
+
+        public int Sum(int number1, int number2)
+        {
+            var result = _applicationFacade.Sum(number1, number2);
+            if(result > 2)
+                throw new Exception("Sum is greater than 2");
+
+            return result;
+        }
+    }
+}
