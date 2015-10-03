e403adae76d68ea1d2767689e60eeca9f83589fb
Gabriel Gonzalez <gabrielnoise@gmail.com>
2015-10-03 22:48:47 +0100

    delete project

--- Base Single Page Application/Demo.Optimization.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Base.SinglePageApplication.MvcApplication" Language="C#" %>
--- Base Single Page Application/Demo.Optimization.Web/Scripts/app/controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    //Controller Creation
-    var homeController = function ($scope) {
-        $scope.numberInStock = 8;
-        $scope.numberOfAuthors = 4;
-    };
-
-    angular.module('BookApp').controller('homeController', ['$scope', homeController]);
-})();
