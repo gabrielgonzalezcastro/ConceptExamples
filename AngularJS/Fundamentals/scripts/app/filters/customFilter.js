645a7446996704ed509603f93005e8f0cea53f6f
Gabriel Gonzalez <gabrielnoise@gmail.com>
2015-10-22 21:14:18 +0100

    initial commit

---
+++ AngularJS/Fundamentals/scripts/app/filters/customFilter.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+
+eventsApp.filter('durations', function() {
+    return function(duration, language) {
+
+        if (language == 'spanish') {
+            switch (duration) {
+            case 1:
+                return "Media Hora";
+            case 2:
+                return "1 Hora";
+            case 3:
+                return "Medio Dia";
+            case 4:
+                return "Todo El Dia";
+            }
+        } else {
+            switch (duration) {
+                case 1:
+                    return "Half Hour";
+                case 2:
+                    return "1 Hour";
+                case 3:
+                    return "Half Day";
+                case 4:
+                    return "Full Day";
+            }
+        }
+    }
+});
+
